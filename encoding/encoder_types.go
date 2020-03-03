586bef5832b8f00fcbb85d148103701172b82e7a
Corey Quon <corey.quon@docker.com>
2020-03-03 01:39:52 -0800

    Add float32 encoder

--- encoding/encoder_types.go
+++ encoding/encoder_types.go
@@ -33,7 +33,9 @@ func newTypeEncoder(t reflect.Type, allowAddr bool) encoderFunc {
 		return intEncoder
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
 		return uintEncoder
-	case reflect.Float32, reflect.Float64:
+	case reflect.Float32:
+		return float32Encoder
+	case reflect.Float64:
 		return floatEncoder
 	case reflect.String:
 		return stringEncoder
@@ -113,6 +115,10 @@ func uintEncoder(v reflect.Value) interface{} {
 
 func floatEncoder(v reflect.Value) interface{} {
 	return v.Float()
+}
+
+func float32Encoder(v reflect.Value) interface{} {
+	return float32(v.Float())
 }
 
 func stringEncoder(v reflect.Value) interface{} {
